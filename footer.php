f7718864a1b3aa042be3d3fbcd0050cf19a8fd57
Kazunari-h <kazunario456@gmail.com>
2020-08-28 20:31:09 +0900

    first commit

---
+++ footer.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package newsroom
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+
+			<div class="footer-menu">
+				<?php dynamic_sidebar( 'sidebar-1' ); ?>
+			</div>
+
+			<a href="https://jura.works">
+				&copy; 2020 K.Hirosawa
+			</a>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
